e852a92d9d6463e7f6ec90e79548f57c8b7fb7bd
Nishil Shah <jay.shah@meghtechnologies.com>
2023-11-01 18:28:49 +0530

    Commit by Office VC

--- index.js
+++ index.js
@@ -2,3 +2,22 @@
 $(function(){
     $("#header").load("_header.html"); 
   });
+
+
+// Only for Trial 
+  //   $('.name-container').click(function(){
+  //     $(this).css("color", "red");
+  // });
+
+
+  // backgroundColor Options = 374931 , 4A785E, Body Color = B4DAA7
+  gsap.to(".main",{
+    backgroundColor: "#374931",
+    scrollTrigger:{
+        trigger:".main",
+        scroller: "body",
+        start:"top -30%",
+        end:"top -100%",
+        scrub:2
+    }
+})
